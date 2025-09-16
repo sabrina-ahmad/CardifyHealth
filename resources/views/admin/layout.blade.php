48c8d04455bf113748eb6c5d73d3275088d967f4
Adnan Ahmed <adnanahmeddev1@gmail.com>
2025-09-16 19:41:59 +0300

    use bootstrap icon cdn

--- resources/views/admin/layout.blade.php
+++ resources/views/admin/layout.blade.php
@@ -29,6 +29,7 @@
             @yield('content')
         </div>
     </div>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous">
     </script>
