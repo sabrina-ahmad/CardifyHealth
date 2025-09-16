cf6ac9311a5cfa5a51cdfa2d673e9eb3db7a4e31
Adnan Ahmed <adnanahmeddev1@gmail.com>
2025-09-16 20:10:04 +0300

    rm vite scss and js

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -7,7 +7,7 @@
 
     <title>Cardify || {{ $title ?? 'Welcome' }}</title>
 
-    @vite(['resources/scss/app.scss', 'resources/js/app.js'])
+    {{-- @vite(['resources/scss/app.scss', 'resources/js/app.js']) --}}
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/custom.css') }}">
