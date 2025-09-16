f8006f529c4015f2f0a09bf3396aa49852168341
Adnan Ahmed <adnanahmeddev1@gmail.com>
2025-09-16 19:06:51 +0300

    users migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -12,13 +12,25 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('users', function (Blueprint $table) {
+            // $table->id();
+            // $table->string('name');
+            // $table->string('email')->unique();
+            // $table->timestamp('email_verified_at')->nullable();
+            // $table->string('password');
+            // $table->rememberToken();
+            // $table->timestamps();
             $table->id();
-            $table->string('name');
+            $table->string('fullname');
+            $table->string('username')->unique(); // <- added username
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('role', ['patient', 'doctor', 'admin'])->default('patient'); // role-based
+            $table->foreignId('hospital_id')->nullable()
+                ->constrained("hospitals")
+                ->onDelete('cascade'); // if doctor
             $table->timestamps();
+            $table->rememberToken();
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
