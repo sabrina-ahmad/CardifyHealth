53c604bd06c23e8ba1e9fc7ef6af4fbf77a6f63e
Adnan Ahmed <adnanahmeddev1@gmail.com>
2025-09-22 14:26:05 +0300

    added 404 page

---
+++ resources/views/errors/404.blade.php
@@ -0,0 +1,10 @@
+<x-layout>
+    <div class="container mt-5 mx-auto d-flex justify-content-center align-items-center" style="min-height: 80vh;">
+        <div class="text-center">
+            <h1 class="display-1 text-danger">404</h1>
+            <h3 class="mb-3">Opps! Page Not Found.</h3>
+            <p class="mb-4">The page you're looking for doesn't exist.</p>
+            <a href="{{ url('/') }}" class="btn btn-primary"><i class="bi bi-arrow-left"></i> Go back to Home</a>
+        </div>
+    </div>
+</x-layout>
