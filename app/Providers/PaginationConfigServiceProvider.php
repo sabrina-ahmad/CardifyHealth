b1e6882e8e27e2a41b867c9da796fced1f69ef97
Adnan Ahmed <adnanahmeddev1@gmail.com>
2025-09-22 16:27:06 +0300

    init pagination config service provider

---
+++ app/Providers/PaginationConfigServiceProvider.php
@@ -0,0 +1,28 @@
+<?
+// app/Providers/PaginationConfigServiceProvider.php
+
+namespace App\Providers;
+
+use Illuminate\Pagination\Paginator;
+use Illuminate\Support\Facades\Config;
+use Illuminate\Support\ServiceProvider;
+
+class PaginationConfigServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        // Set pagination view
+        Config::set('pagination.view', 'pagination::bootstrap-5');
+
+        // Set items per page
+        Config::set('pagination.per_page', 10);
+
+        // Register pagination views
+        $this->loadViewsFrom(resource_path('views/vendor/pagination'), 'pagination');
+    }
+
+    public function register()
+    {
+        //
+    }
+}
