e22e3fb1edce3ea5c496c9d5b78fa09c7f397572
Adnan Ahmed <adnanahmeddev1@gmail.com>
2025-09-22 17:07:22 +0300

    replace icon activity with svg acitvity

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -64,7 +64,11 @@
                 <div class="card col-lg-3 col-md-3 col-sm-12 border-info shadow">
                     <div class="card-body my-3">
                         <span class=" text-light btn-gradient-primary py-3 px-3 text-start rounded-3">
-                            <i class="bi bi-activity"></i>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor"
+                                class="bi bi-activity" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                    d="M6 2a.5.5 0 0 1 .47.33L10 12.036l1.53-4.208A.5.5 0 0 1 12 7.5h3.5a.5.5 0 0 1 0 1h-3.15l-1.88 5.17a.5.5 0 0 1-.94 0L6 3.964 4.47 8.171A.5.5 0 0 1 4 8.5H.5a.5.5 0 0 1 0-1h3.15l1.88-5.17A.5.5 0 0 1 6 2" />
+                            </svg>
                         </span>
                         <h5 class="card-title mt-3">Real-Time Dashboards</h5>
                         <p class="card-text text-body-secondary">Comprehensive dashboards with key metrics and activity
