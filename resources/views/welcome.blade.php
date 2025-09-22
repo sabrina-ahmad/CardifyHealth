e36657a2fcd60564706b7b6ac3bcfb4bff114d2f
Adnan Ahmed <adnanahmeddev1@gmail.com>
2025-09-22 16:32:02 +0300

    added default notify

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -420,4 +420,59 @@
 
 
     </main>
+    <script src="{{ asset('js/toast.js') }}"></script>
+
+    @if (session('success'))
+        <div class="notify-container" id="notifyContainer">
+        </div>
+        <script>
+            // showToast("{{ session('success') }}", "info")
+            showNotify(
+                "<i class='bi bi-check-circle-fill text-white'></i> <span class='text-white'>{{ session('success') }}<span>",
+                'info', 5000)
+        </script>
+    @endif
+
+    @if (session('info'))
+        <div class="notify-container" id="notifyContainer">
+        </div>
+        <script>
+            // showToast("{{ session('success') }}", "info")
+            showNotify(
+                "<i class='bi bi-door-open text-white'></i> <span class='text-white'>{{ session('info') }}<span>",
+                'success', 5000)
+        </script>
+    @endif
+
+    @if (session('error'))
+        <div class="notify-container" id="notifyContainer">
+        </div>
+        <script>
+            // showToast("{{ session('error') }}", "error")
+            showNotify("<i class='bi bi-x-circle text-dark'></i> <span class='text-dark'>{{ session('error') }}<span>",
+                'error', 5000)
+        </script>
+    @endif
+
+    @if (session('warning'))
+        <div class="notify-container" id="notifyContainer">
+        </div>
+        <script>
+            // showToast("{{ session('warning') }}", "warning")
+            showNotify(
+                "<i class='bi bi-exclamation-circle-fill text-dark'></i> <span class='text-dark'>{{ session('warning') }}<span>",
+                'warning', 5000)
+        </script>
+    @endif
+
+    @if ($errors->any())
+        <div class="notify-container" id="notifyContainer">
+        </div>
+
+        <script>
+            showNotify(
+                "<strong>Error!</strong> <br> <ul> @foreach ($errors->all() as $error) <li>{{ $error }}</li> @endforeach </ul>",
+                "error", 5000)
+        </script>
+    @endif
 </x-layout>
